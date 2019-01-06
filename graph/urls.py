fbb05620aa41b204b466fbd13ffa2de9704f398d
bor1e <ilija.borodin@fau.de>
2019-01-06 18:48:56 +0100

    initialzed graph based on domains

--- graph/urls.py
+++ graph/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # 127.0.0.1:8000/graph/
     # path('', views.index, name='complete_graph'),
     path('<domain>', views.index, name='show_graph'),
+    path('init/<domain>', views.init_graph, name='init_graph'),
     path('api/', views.api, name='api'),
     path('test/', views.test, name='test'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-import json
-from django.http import JsonResponse, HttpResponse
-from start.models import Domains
+from django.http import JsonResponse
 from filter.models import BlackList
+from start.models import Domains, Externals
 from urllib.parse import urlparse
 
 import logging
@@ -20,12 +19,11 @@ def index(request, domain):
     non_filtered = [obj.external_domain for obj in domain.externals.all()
                     if obj.external_domain not in filtered]
 
-    logger.debug('external.non_filtered: %s' % len(non_filtered))
-
     displaying = [obj.domain for obj in Domains.objects.all()
                   if obj.domain in non_filtered and obj.fullscan]
 
-    logger.debug('external.displaying: %s' % len(displaying))
+    logger.debug('external.displaying %s from external.non_filtered %s' %
+                 (len(displaying), len(non_filtered)))
 
     stats = {
         'domain': domain.domain,
@@ -35,6 +33,70 @@ def index(request, domain):
         'rest': domain.externals.count() - len(displaying),  # .count()
     }
     return render(request, 'graph.html', {'stats': stats})
+
+
+def init_graph(request, domain):
+    db_domain = Domains.objects.get(domain=domain)
+    filtered = [obj.external_domain for obj in db_domain.externals.all()
+                if obj.external_domain in
+                BlackList.objects.values_list('ignore', flat=True)]
+    non_filtered = [obj.external_domain for obj in db_domain.externals.all()
+                    if obj.external_domain not in filtered]
+    # need to add the domain for which the graph was called, because it is not
+    # included in the non_filtered list, because it is based on the domain...
+    non_filtered.append(domain)
+    remaining = Domains.objects.all().filter(domain__in=non_filtered,
+                                             fullscan=True)
+    nodes = list()
+    edges = list()
+    domains_counter = initialize_domains(remaining)
+    ''' TODO:
+    - extract www. from domain prefix
+    --> do it on DB level (Domains & Externals)
+    --> add spider start_urls & allowed_domains
+    OR
+    --> when calculating IGNORE www while initializing domains_counter
+    '''
+    logger.debug('initialized domains_counter: %s' % domains_counter)
+    edges_counter = list()
+    for d in remaining:
+        node = {
+            'id': d.domain,
+            # size depends on externals sites pointing to that one.
+            'size': domains_counter[d.domain] * 3 \
+            if domains_counter[d.domain]\
+            else 1,
+            'label': d.domain
+        }
+        nodes.append(node)
+        for e in d.externals.all():
+            e_domain = e.external_domain
+            if e_domain not in remaining.values_list('domain', flat=True):
+                continue
+            # can be seperated if we want a bidrictional graph
+            # creating a structure in the dicts:
+            edge_id = node['id'] + '_' + e_domain if node['id'] < e_domain \
+                else e_domain + '_' + node['id']
+            if edge_id not in edges_counter:
+                edges_counter.append(edge_id)
+                edge = {
+                    # TODO to be unique if we want to have different directions
+                    'id': edge_id,  # + '_' + j
+                    'source': node['id'],
+                    'target': Domains.objects.get(domain=e_domain).domain,
+                    'size': 1
+                }
+                edges.append(edge)
+            else:
+                # i.e. there exists already a connection between those domains
+                for edge in edges:
+                    if edge['id'] == edge_id:
+                        edge['size'] += 1
+                        break
+
+    data = {'nodes': nodes, 'edges': edges}
+
+    return JsonResponse({'data': data})
 
 
 def api(request, domain=None):
@@ -84,6 +146,30 @@ def api(request, domain=None):
     data = {'nodes': nodes, 'edges': edges}
 
     return JsonResponse({'data': data})
+
+
+def initialize_domains(domains):
+    domains_list = [d.domain for d in domains]
+    domains_size = {}
+    for d in domains_list:
+        domains_size[d] = 0
+
+    '''
+    # in this case the counter of domains linked to is only based on the
+    # selected domains for the fullscan
+    # the loop below search the entire DB
+    for d in domains:
+        for e in d.externals.all():
+            if e.external_domain in domains_size:
+                domains_size[e.external_domain] += 1
+    '''
+
+    # in this case the counter of domains checks all the db for given domain
+    for e in Externals.objects.all():
+        if e.external_domain in domains_size:
+            domains_size[e.external_domain] += 1
+
+    return domains_size
 
 
 def test(request):
