b6fdc355344824d0613c62c8d77e51114c45a2bd
bor1e <ilija.borodin@fau.de>
2019-01-13 18:04:40 +0100

    finished editable cells

--- start/urls.py
+++ start/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000/api/
     path('', views.index, name='start'),
+    path('edit/<domain>/zip/<external>', views.edit_zip, name='edit_zip'),
+    path('edit/<domain>/name/<external>', views.edit_name, name='edit_name'),
 ]
--- start/views.py
+++ start/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from start.models import Domains
+from django.core.exceptions import ObjectDoesNotExist
 
 import logging
 logger = logging.getLogger(__name__)
@@ -10,3 +11,36 @@ def index(request):
     ds = Domains.objects.all()
 
     return render(request, 'index.html', {'domains': ds})
+
+
+def edit_zip(request, domain, external):
+    zipcode = request.POST.get('edit_zip').strip()
+    if not zipcode or not zipcode.isdigit():
+        # TODO error message
+        return redirect('display', domain=domain)
+
+    try:
+        db_domain = Domains.objects.get(domain=external)
+    except ObjectDoesNotExist:
+        logger.debug('not found : %s' % domain)
+        db_domain = Domains.objects.filter(domain__icontains=external).first()
+    db_domain.info.zip = zipcode
+    db_domain.info.save()
+    return redirect('display', domain=domain)
+
+
+def edit_name(request, domain, external):
+    logger.debug(request.POST)
+    name = request.POST.get('edit_name').strip()
+    if not name:
+        # TODO error message
+        return redirect('display', domain=domain)
+
+    try:
+        db_domain = Domains.objects.get(domain=external)
+    except ObjectDoesNotExist:
+        logger.debug('not found : %s' % external)
+        db_domain = Domains.objects.filter(domain__icontains=external).first()
+    db_domain.info.name = name
+    db_domain.info.save()
+    return redirect('display', domain=domain)
